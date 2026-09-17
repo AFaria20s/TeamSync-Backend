8abb82a4f476a6fc9287eb04a113d45adb250234
AfonsoFaria20s <affaria188@gmail.com>
2026-09-17 13:52:03 +0100

    Deleted old documentation. Updated security and other project info

--- src/main/java/org/afonso/teamsync/security/SecurityConfig.java
+++ src/main/java/org/afonso/teamsync/security/SecurityConfig.java
@@ -31,6 +31,11 @@ public class SecurityConfig {
                 .cors(cors -> cors.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers(
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
